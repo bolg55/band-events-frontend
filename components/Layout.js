8075e4800f74ec0beb60c28966accde8490f3abd
Kellen Bolger <bolger.kellen@gmail.com>
2021-06-29 23:26:57 -0400

    added Header and Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,25 @@
-import Head from 'next/head'
-import styles from '../styles/Layout.module.css'
+import Head from "next/head"
+import Header from "./Header"
+import Footer from "./Footer"
+import styles from "../styles/Layout.module.css"
 
-export default function Layout({title, keywords, description, children}) {
-    return (
-        <div>
-        <Head>
-            <title>{title}</title>
-            <meta name='description' content={description} />
-            <meta name = 'keywords' content = {keywords} />
-        </Head>
-        <div className={styles.container}>
-        {children}
-        </div>
-            
-        </div>
-    )
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+      <Header />
+      <div className={styles.container}>{children}</div>
+      <Footer />
+    </div>
+  )
 }
 
-
 Layout.defaultProps = {
-    title: 'Concerts & Events | Find the hottest parties',
-    description : 'Find the latest concerts and events',
-    keywords: 'music, rock, dj, bands, country, edm, pop, electronic, events'
-
+  title: "Concerts & Events | Find the hottest parties",
+  description: "Find the latest concerts and events",
+  keywords: "music, rock, dj, bands, country, edm, pop, electronic, events",
 }
